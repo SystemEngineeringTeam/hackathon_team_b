d2d16460eb5bf063000d74b3706ebd7cf34e230d
hihumikan <musecharo@gmail.com>
2021-03-15 12:41:21 +0900

    a

---
+++ vue/src/main.js
@@ -0,0 +1,19 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/tailwind.css'
+import vuetify from './plugins/vuetify';
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
