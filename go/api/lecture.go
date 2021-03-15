bd1d86d4f2896eac66ffda9132216581a52b3390
k20109kk <k20109kk@aitech.ac.jp>
2021-03-15 23:08:00 +0900

    修正中

--- go/api/lecture.go
+++ go/api/lecture.go
@@ -22,41 +22,22 @@ func Lectures(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 
 	db.CallRectures(" ", " ", " ", " ", " ", " ", " ")
-	//Get
-	if r.Method == http.MethodGet {
-
-	}
-
 	url := "https://jsonplaceholder.typicode.com/todos"
 
-	request, err := http.NewRequest("GET", url, nil)
+	r, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	//クエリパラメータ
-	params := request.URL.Query()
+	params := r.URL.Query()
 	params.Add("userId", "1")
-	request.URL.RawQuery = params.Encode()
 
-	fmt.Println(request.URL.String()) //https://jsonplaceholder.typicode.com/todos?userId=1
-
-	timeout := time.Duration(5 * time.Second)
-	client := &http.Client{
-		Timeout: timeout,
+	fmt.Println(r.URL.String())
 	}
 
-	response, err := client.Do(request)
-	if err != nil {
-		log.Fatal(err)
+	//Get
+	if r.Method == http.MethodGet {
+
 	}
 
-	defer response.Body.Close()
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(body))
-}
