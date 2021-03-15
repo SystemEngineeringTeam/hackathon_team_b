a6ab00adbe441f40127d7a4a8358c1b61fc2bec8
happy663 <53854298+happy663@users.noreply.github.com>
2021-03-15 15:16:27 +0900

    goディレクトリ設定

---
+++ go/api/lecture.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+//Test はテストする関数
+func Test(){
+	fmt.Println("test")
+}
+
+func Lectures(w http.ResponseWriter,r *http.Request){
+
+	//セキリティ設定
+	w.Header().Set("Access-Control-Allow-Origin", "*")                       // Allow any access.
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE") // Allowed methods.
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+
+	//Get
+	if r.Method==http.MethodGet{
+
+	}
+
+
+}
+
+
+
+
---
+++ go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+	"with_b/api"
+)
+
+func main()  {
+	api.Test()
+
+	http.HandleFunc("/lecture",api.Lectures)
+	http.ListenAndServe(":8080",nil)
+}
