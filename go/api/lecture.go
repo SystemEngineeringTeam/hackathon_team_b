4d55ee39489d8cc5453840714e94c5a27bd9e1e9
k20109kk <k20109kk@aitech.ac.jp>
2021-03-16 16:11:26 +0900

    get処理

--- go/api/lecture.go
+++ go/api/lecture.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"with_b/db"
 )
 
@@ -19,7 +21,40 @@ func Lectures(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE") // Allowed methods.
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 
-	db.CallRectures(" ", " ", " ", " ", " ", " ", " ")
+	//クエリパラメータ
+	params := r.URL.Query()
+	// params.Add("grade", "3")
+	// params.Add("department", "kk")
+	// fmt.Println(params["grade"][0])
+	// fmt.Println(params["department"][0])
+	// fmt.Println(params)
+
+	//Get
+	if r.Method == http.MethodGet {
+
+		//recsは構造体のスライス
+		recs, err := db.CallRectures(params["grade"][0], params["department"][0], params["semester"][0], params["dayofWeek"][0], params["time"][0], params["teacher"][0], params["lectureName"][0])
+
+		//エラー処理
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		//byte型に変換する
+		recBytes, err := json.Marshal(recs)
+
+		//エラー処理
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		//stringに変換
+		stringrecs := string(recBytes)
+		fmt.Fprintln(w, stringrecs)
+	}
+
 	url := "https://jsonplaceholder.typicode.com/todos"
 
 	r, err := http.NewRequest("GET", url, nil)
@@ -28,16 +63,13 @@ func Lectures(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//lecture?grade="3"&department="kk"
-	//クエリパラメータ
-	params := r.URL.Query()
-	params.Add("grade", "3")
-	params.Add("department", "kk")
-	fmt.Println(params["grade"][0])
-	fmt.Println(params["department"][0])
 
 }
 
-//Get
-// if r.Method == http.MethodGet {
-
-// }
+func toJSON(r interface{}) string {
+	js, err := json.Marshal(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.ReplaceAll(string(js), ",", ",")
+}
