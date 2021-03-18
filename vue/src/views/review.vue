e8574840a5687abb0a900dc8ee9a86328ba15709
hihumikan <musecharo@gmail.com>
2021-03-18 15:47:24 +0900

    あ

---
+++ vue/src/views/review.vue
@@ -0,0 +1,25 @@
+<template></template>
+
+<script>
+export default {
+  /*
+  registerReview() {
+    axios
+      .post("http://localhost:3030/review", {
+        IndexLectureNumber: 4,
+        ReviewStar: 5,
+        sentence: "登録完了",
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+  */
+};
+</script>
+
+<style>
+</style>
