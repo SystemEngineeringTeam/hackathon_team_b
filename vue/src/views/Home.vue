f4cf41f49e9d769d75753fc7ce2c91c339c1b72f
hihumikan <musecharo@gmail.com>
2021-03-19 10:38:31 +0900

    ボタン修正

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,119 +1,114 @@
 <template>
   <div id="app">
     <v-app>
-    <v-container class="mt-6">
-      <!-- 最大画面の表示は正常だが、中画面〜スマホサイズのレスポンシブは最悪
+      <v-container class="mt-6">
+        <!-- 最大画面の表示は正常だが、中画面〜スマホサイズのレスポンシブは最悪
       最大画面の表示で正常な処理ができれば、後からでも中画面のレスポンシブは変えれるので
       今は処理が出来ることを目指す。
       -->
-      <v-row class="lighten-3" style="height: 200px">
-        <v-col cols="12" sm="8" md="6" lg="4" xl="3">
-          <b-card title="">
-            <p class="text-3xl">学期</p>
-            <v-chip-group v-model="semester" column>
-              <v-chip filter outlined>前期</v-chip>
-              <v-chip filter outlined>後期</v-chip>
-            </v-chip-group>
-            <div class="mt-2">
-              Selected: <strong>{{ semester }}</strong>
-            </div>
-          </b-card>
-        </v-col>
-        <v-col cols="12" sm="8" md="6" lg="4" xl="3">
-          <b-card title="">
-            <p class="text-3xl">学年</p>
-            <b-form-select
-              v-model="gradeSelected"
-              :options="gradeOptions"
-            ></b-form-select>
-            <div class="mt-3">
-              Selected: <strong>{{ gradeSelected }}</strong>
-            </div>
-          </b-card>
-        </v-col>
-        <v-col cols="12" sm="8" md="6" lg="4" xl="3">
-          <b-card title="">
-            <p class="text-3xl">時限</p>
-            <b-form-select
-              v-model="timeSelected"
-              :options="timeOptions"
-            ></b-form-select>
-            <div class="mt-3">
-              Selected: <strong>{{ timeSelected }}</strong>
-            </div>
-          </b-card>
-        </v-col>
-        <v-col cols="12" sm="8" md="6" lg="4" xl="3">
-          <b-card title="">
-            <p class="text-3xl">曜日</p>
-            <b-form-select
-              v-model="dayofweekSelected"
-              :options="dayofweekOptions"
-            ></b-form-select>
-            <div class="mt-3">
-              Selected: <strong>{{ dayofweekSelected }}</strong>
-            </div>
-          </b-card>
-        </v-col>
-        <v-col cols="12" sm="8" md="6" lg="4" xl="3">
-          <b-card title="">
-            <p class="text-3xl">専攻</p>
-            <b-form-select
-              v-model="DepartmentSelected"
-              :options="DepartmentOptions"
-            ></b-form-select>
-            <div class="mt-3">
-              Selected: <strong>{{ DepartmentSelected }}</strong>
-            </div>
-          </b-card>
-        </v-col>
-        <v-col cols="12" sm="8" md="6" lg="4" xl="3">
-          <b-card title="">
-            <p class="text-3xl">講師</p>
-            <div>
-              <b-form-textarea
-                id="textarea"
-                v-model="teacherText"
-                placeholder="Enter something..."
-                rows="1"
-                max-rows="1"
-              ></b-form-textarea>
-            </div>
-            <div class="mt-3">
-              Selected: <strong>{{ teacherText }}</strong>
-            </div>
-          </b-card>
-        </v-col>
-        <v-col cols="12" sm="8" md="6" lg="4" xl="3">
-          <b-card title="">
-            <p class="text-3xl">科目名</p>
-            <div>
-              <b-form-textarea
-                id="lectureName"
-                v-model="lectureName"
-                placeholder="Enter something..."
-                rows="1"
-                max-rows="1"
-              ></b-form-textarea>
-            </div>
-            <div class="mt-3">
-              Selected: <strong>{{ lectureName }}</strong>
-            </div>
-          </b-card>
-        </v-col>
-        <v-btn
-      depressed
-      @click="Clicksubmit(), lectureGetParams()"
-      color="primary"
-    >
-      検索
-    </v-btn>
-      </v-row>
-
-    </v-container>
-    <div class="float-right m-10">
-      <!-- <button @click="urlpush()">push</button> -->
-    </div>
+        <v-row class="lighten-3" style="height: 200px">
+          <v-col cols="12" sm="8" md="6" lg="4" xl="3">
+            <b-card title="">
+              <p class="text-3xl">学期</p>
+              <v-chip-group v-model="semester" column>
+                <v-chip filter outlined>前期</v-chip>
+                <v-chip filter outlined>後期</v-chip>
+              </v-chip-group>
+              <div class="mt-2">
+                Selected: <strong>{{ semester }}</strong>
+              </div>
+            </b-card>
+          </v-col>
+          <v-col cols="12" sm="8" md="6" lg="4" xl="3">
+            <b-card title="">
+              <p class="text-3xl">学年</p>
+              <b-form-select
+                v-model="gradeSelected"
+                :options="gradeOptions"
+              ></b-form-select>
+              <div class="mt-3">
+                Selected: <strong>{{ gradeSelected }}</strong>
+              </div>
+            </b-card>
+          </v-col>
+          <v-col cols="12" sm="8" md="6" lg="4" xl="3">
+            <b-card title="">
+              <p class="text-3xl">時限</p>
+              <b-form-select
+                v-model="timeSelected"
+                :options="timeOptions"
+              ></b-form-select>
+              <div class="mt-3">
+                Selected: <strong>{{ timeSelected }}</strong>
+              </div>
+            </b-card>
+          </v-col>
+          <v-col cols="12" sm="8" md="6" lg="4" xl="3">
+            <b-card title="">
+              <p class="text-3xl">曜日</p>
+              <b-form-select
+                v-model="dayofweekSelected"
+                :options="dayofweekOptions"
+              ></b-form-select>
+              <div class="mt-3">
+                Selected: <strong>{{ dayofweekSelected }}</strong>
+              </div>
+            </b-card>
+          </v-col>
+          <v-col cols="12" sm="8" md="6" lg="4" xl="3">
+            <b-card title="">
+              <p class="text-3xl">専攻</p>
+              <b-form-select
+                v-model="DepartmentSelected"
+                :options="DepartmentOptions"
+              ></b-form-select>
+              <div class="mt-3">
+                Selected: <strong>{{ DepartmentSelected }}</strong>
+              </div>
+            </b-card>
+          </v-col>
+          <v-col cols="12" sm="8" md="6" lg="4" xl="3">
+            <b-card title="">
+              <p class="text-3xl">講師</p>
+              <div>
+                <b-form-textarea
+                  id="textarea"
+                  v-model="teacherText"
+                  placeholder="Enter something..."
+                  rows="1"
+                  max-rows="1"
+                ></b-form-textarea>
+              </div>
+              <div class="mt-3">
+                Selected: <strong>{{ teacherText }}</strong>
+              </div>
+            </b-card>
+          </v-col>
+          <v-col cols="12" sm="8" md="6" lg="4" xl="3">
+            <b-card title="">
+              <p class="text-3xl">科目名</p>
+              <div>
+                <b-form-textarea
+                  id="lectureName"
+                  v-model="lectureName"
+                  placeholder="Enter something..."
+                  rows="1"
+                  max-rows="1"
+                ></b-form-textarea>
+              </div>
+              <div class="mt-3">
+                Selected: <strong>{{ lectureName }}</strong>
+              </div>
+            </b-card>
+          </v-col>
+        </v-row>
+      </v-container>
+      <div class="absolute right-2 bottom-2">
+        <v-btn color="primary" @click="Clicksubmit(), lectureGetParams()" fab x-large dark>
+          <v-icon>mdi-layers-search</v-icon>
+        </v-btn>
+        <!-- <button @click="urlpush()">push</button> -->
+      </div>
     </v-app>
   </div>
 </template>
